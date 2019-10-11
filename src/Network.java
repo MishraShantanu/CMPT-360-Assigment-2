e9ef32e62d3edddffc19933af4cc7236fa7bf517
halfershan33 <49019461+halfershan33@users.noreply.github.com>
2019-10-11 16:02:53 -0600

    adding for loop n making Algo n^2

--- src/Network.java
+++ src/Network.java
@@ -32,21 +32,13 @@ public class Network {
         //start: edit and write your code here
         int test = 0;
         for (int i = 0; i < n; i++) {
-            //int []arr = new int[n-(i+1)];
-//		    System.arraycopy(D,i+1,arr,0,(n-(i+1)));
-//		    Arrays.sort(arr);
-//            if (i < n - 1) {
-//                if(test<D[i]-arr[0]){
-//		        test=D[i]-arr[0];
-//            }
-            for (int j = i+1; j < n; j++) {
-                if (test < D[i] - D[j]) {
-                    test = D[i] - D[j];
+            int j=i+1;
+            while(j <n){
+                if(test<D[i]-D[j]){
+                    test=D[i]-D[j];
                 }
+                j++;
             }
-
-
-
         }
         return test;
         //end: write your code here
